f647dc59abab952c1b27e9ab377f3f28cb8376cd
RebecaBA <rebebolaos.98@gmail.com>
2022-03-06 21:22:41 -0600

    React introdution and examples

--- src/App.js
+++ src/App.js
@@ -2,22 +2,41 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const blockInfo = [
+    {
+      title: 'Card 1',
+      description: 'Learn React JS from scratch with hands-on practice assignments and projects.',
+      index: 1,
+    },
+    {
+      title: 'Card 2',
+      description: 'Learn React JS from scratch with hands-on practice assignments and projects.',
+      index: 2,
+    },
+    {
+      title: 'Card 3',
+      description: 'Learn React JS from scratch with hands-on practice assignments and projects.',
+      index: 3,
+    },
+    {
+      title: 'Card 4',
+      description: 'Learn React JS from scratch with hands-on practice assignments and projects.',
+      index: 4,
+    },
+    {
+      title: 'Card 5',
+      description: 'Learn React JS from scratch with hands-on practice assignments and projects.',
+      index: 5,
+    }
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {blockInfo && blockInfo.map((blog) => (
+        <div className="blog-card" key={blog.index}>
+          <h3>{blog.title}</h3>
+          <div>{blog.description}</div>
+        </div>
+      ))}
     </div>
   );
 }
